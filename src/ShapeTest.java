001bae3c64cf0011e35889c8258e3a274417cbe3
SergiyBoiko <codedva00@gmail.com>
2016-10-31 21:51:56 +0200

    add main method

--- src/ShapeTest.java
+++ src/ShapeTest.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
+
+import Shape.*;
 
 public class ShapeTest {
-
+	
+	private static Scanner input;
+	
+	public static void main(String[] args){
+		Shape shape = CreateShapeInMemory.createShape(input = new Scanner(System.in));
+		CheckInputNameShape.infoShape(shape);
+	}
 }
